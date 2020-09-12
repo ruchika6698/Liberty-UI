e0fa82f0fbb30d3f53fcd573177de9a24d5239b4
ruchika6698 <ruchika6698@gmail.com>
2020-09-12 18:38:24 +0530

    Added:Logo and user Icon assets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navigationbar from './Components/Navigationbar'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="navigationBar">
+          <Navigationbar/>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
