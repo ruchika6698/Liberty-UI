59f2521ddbd6f0c2ddca56820c864e3c8fa9360d
ruchika6698 <ruchika6698@gmail.com>
2020-09-15 20:38:30 +0530

    Refactor:Summary table

--- src/Components/TableSummary.jsx
+++ src/Components/TableSummary.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "../SCSS/Table.scss";
-import Table from 'react-bootstrap/Table'
 import Button from "react-bootstrap/Button";
 
 export default function TableSummary() {
   return (
-    <div>
-      <Table responsive striped bordered hover>
+    <div className="tableContainer">
+      <table>
         <thead>
-          <tr>
+          <tr className="tableHead">
             <th>ID</th>
             <th>Assignee</th>
             <th>Task Details</th>
@@ -18,54 +17,53 @@ export default function TableSummary() {
             <th>Tracking Number</th>
           </tr>
         </thead>
-        <br/>
         <tbody>
           <tr>
-            <th>#320</th>
-            <th>Mark C.Diaz</th>
-            <th>Support of thteme</th>
-            <th>Credit card</th>
-            <th>
-              <Button variant="success">Approved</Button>{" "}
-            </th>
-            <th>$12,245</th>
-            <th>JPBBN435893458</th>
+            <td>#320</td>
+            <td>Mark C.Diaz</td>
+            <td>Support of thteme</td>
+            <td>Credit card</td>
+            <td>
+              <Button className="approved">Approved</Button>{" "}
+            </td>
+            <td>$12,245</td>
+            <td>JPBBN435893458</td>
           </tr>
           <tr>
-            <th>#321</th>
-            <th>Jose D</th>
-            <th>Verify your email address !</th>
-            <th>Internet banking</th>
-            <th>
-              <Button background-color="orange">Pending</Button>{" "}
-            </th>
-            <th>$12,245</th>
-            <th>BDYBN435893325</th>
+            <td>#321</td>
+            <td>Jose D</td>
+            <td>Verify your email address !</td>
+            <td>Internet banking</td>
+            <td>
+              <Button className="pending">Pending</Button>{" "}
+            </td>
+            <td>$12,245</td>
+            <td>BDYBN435893325</td>
           </tr>
           <tr>
-            <th>#322</th>
-            <th>Philips T</th>
-            <th>Item support message send</th>
-            <th>Credit card</th>
-            <th>
-              <Button variant="success">Approved</Button>{" "}
-            </th>
-            <th>$12,245</th>
-            <th>JSNTN435884258</th>
+            <td>#322</td>
+            <td>Philips T</td>
+            <td>Item support message send</td>
+            <td>Credit card</td>
+            <td>
+              <Button className="approved">Approved</Button>{" "}
+            </td>
+            <td>$12,245</td>
+            <td>JSNTN435884258</td>
           </tr>
           <tr>
-            <th>#323</th>
-            <th>Luke Pixel</th>
-            <th>New submission on website</th>
-            <th>Cash on delivery</th>
-            <th>
-              <Button variant="danger">Rejected</Button>{" "}
-            </th>
-            <th>$12,245</th>
-            <th>JPABT435893678</th>
+            <td>#323</td>
+            <td>Luke Pixel</td>
+            <td>New submission on website</td>
+            <td>Cash on delivery</td>
+            <td>
+              <Button className="danger">Rejected</Button>{" "}
+            </td>
+            <td>$12,245</td>
+            <td>JPABT435893678</td>
           </tr>
         </tbody>
-      </Table>
+      </table>
     </div>
   );
 }
