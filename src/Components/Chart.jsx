5a1e4e59dc846361c62b6938b02ba4a7a7c2266d
ruchika6698 <ruchika6698@gmail.com>
2020-09-16 14:32:05 +0530

    Added:Line graph in Chart component

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -3,8 +3,120 @@ import "../SCSS/Chart.scss";
 import Maria from "./../Assets/Maria.jpg";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import {Line} from 'react-chartjs-2'
 
 export default function Usersfeed() {
+
+    const data = {
+            labels:['2013','2014','2014','2015','2016','2017','2018'],
+            datasets:[
+                {
+                label:'month',
+
+                data: [1,2,1,2.5,3.5,3,4],
+                borderColor:['#4D54E5'],
+                borderWidth:['thin'],
+                backgroundColor:['#ADB4F5'],
+                pointBackgroundColor:['#4D54E5'],
+                pointBorderColor:['#4D54E5']
+
+              
+            },
+                {
+                    data: [3,5,4,6,8,7,8.5],
+                    borderColor:['#4D54E5'],
+                    borderWidth:['thin'],
+                    backgroundColor:['#D9E1FD'],
+                    pointBackgroundColor:['#D9E1FD'],
+                    pointBorderColor:['#4D54E5']
+                }
+            ]
+        }
+    const option={
+    
+        legend: {
+            display: false
+        },
+        scales:{
+            xAxes:[{
+                ticks: {
+                    backdropColor : "rgba(255,255,255,0)",
+                   
+                    display: false //this will remove only the label
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false,
+                  },
+            }],
+           yAxes:[{
+            ticks: {
+                backdropColor : "rgba(255,255,255,0)",
+                max: 10,
+            min: 0,
+                stepSize: 1,
+                display: false //this will remove only the label
+            },
+               
+                gridLines: {
+                    display: false,
+                    drawBorder: false,
+                  },
+               
+           }]
+        }    
+    }
+
+    const data1 = {
+        labels:['2006','2007','2008','2009','2010','2011'],
+        datasets:[
+            {
+                data: [50,75,27,30,63,76],
+                borderColor:['#6970EA'],
+                backgroundColor:['white'],
+   
+                pointBackgroundColor:['#A8BBFF'],
+                pointBorderColor:['white']
+            },
+            {
+                data: [0,78,10,50,100,62],
+                borderColor:['#D9E1FD'],
+            
+                backgroundColor:['white'],
+                pointBackgroundColor:['#30335C'],
+                pointBorderColor:['white']
+           }
+        ]
+    }
+    const options={
+
+        legend: {
+            display: false
+        },
+        scales:{
+        xAxes:[{
+        
+            gridLines: {
+                display: false,
+                drawBorder: false,
+            },
+        }],
+        yAxes:[{
+        ticks: {
+            max: 100,
+            min: 0,
+            stepSize: 25,
+            Display: false //this will remove only the label
+        },
+       
+        gridLines: {
+            display: false,
+            drawBorder: false,
+        },
+        }]
+    }
+    }
+
 
   return (
     <div className="Chart">
@@ -39,7 +151,8 @@ export default function Usersfeed() {
         </div>
         <div>
             <Card className="card">
-                <div className="graphCard">
+                <div >
+                    <div className="graphCard">
                     <div className="product">Top Products</div>
                     <div className="value">598,486</div>
                     <div >6.5% change from today</div>
@@ -48,8 +161,9 @@ export default function Usersfeed() {
                     <div className="product">Support Cases</div>
                     <div className="value">323,360</div>
                     <div >2.5% change from yesterday</div>
-                    <div>
-                    
+                    </div>
+                    <div >
+                        <Line data={data} options={option}  height="190px" style={{width:'100%'}}/>
                     </div>
                 </div>
             </Card>
@@ -73,7 +187,10 @@ export default function Usersfeed() {
                         Open Campaign
                     </div>
                 </div>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <div className>
+                    <Line data={data1} options={options} height='250px' style={{ width:'100%'}} />
+                </div>
+                <br/>
                 <div className="sales">
                     <div className="campaign">
                         5136
